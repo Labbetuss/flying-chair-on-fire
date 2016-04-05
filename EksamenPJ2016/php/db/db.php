a8310b719bad3f357b28f4ca097c2ae9b2ed9975
Labbetuss <Danieldavidsen@gmail.com>
2016-04-05 16:18:48 +0200

    Da skal php scripts være på plass

---
+++ EksamenPJ2016/php/db/db.php
@@ -0,0 +1,15 @@
+<?php
+
+
+  $host = "localhost";
+  $database = "PJEksamen";
+  $user = "daniel";
+  $password = "davidsen";
+
+  try {
+      $dbConn = new PDO("mysql:host=$host; dbname=$database", "$user", "$password");
+      $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  } catch(PDOException $e) {
+    echo "Feil ved tilkobling mot databasen, spør Daniel om hjelp" . $e->getMessage();
+    exit();
+  }
