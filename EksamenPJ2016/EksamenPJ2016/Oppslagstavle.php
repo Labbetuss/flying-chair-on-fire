b7db6ba4a234b11f2ba8e90ada7f341aab88e934
sillyweee <johsil15@student.westerdals.no>
2016-04-07 16:38:53 +0200

    Added files via upload

---
+++ EksamenPJ2016/EksamenPJ2016/Oppslagstavle.php
@@ -0,0 +1,53 @@
+<!-- av: Silje Lilleeng Johnsen-->
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Oppslagstavle</title>
+        <link rel="stylesheet" type="text/css" href="Styles/Oppslag.css">
+        <link rel="stylesheet" href="Styles/Responsive.css" media="screen and (max-width:1200px)">
+    </head>
+    <body>
+                <?php
+include 'Templateutencont.php';
+?>
+        <div class="boks-innhold">
+                <div class="trestk">
+        <div class="tredjedel kollaps">
+            <img src="Bilder/IS09AR7JD_Comp.jpg" alt="Eksempelbilde"/>
+            <h2>Jeg ønsker!!!</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                dictum sodales tempus. Pellentesque fringilla ex a malesuada 
+                dictum. Proin enim purus, consectetur non blandit in, vehicula 
+                sit amet sapien. Vestibulum dictum augue id commodo aliquam. 
+                Integer aliquet iaculis dapibus. Pellentesque est sapien, 
+                imperdiet non mi ut, rutrum commodo nisi. Mauris gravida 
+                convallis mauris, vitae posuere mauris rhoncus vitae. </p>
+        </div>
+                    
+        <div class="tredjedel tredjedel-andre kollaps">
+            <img src="Bilder/IS09AR7JD_Comp.jpg" alt="Eksempelbilde"/>
+            <h2>Prosjektoppstart</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                dictum sodales tempus. Pellentesque fringilla ex a malesuada 
+                dictum. Proin enim purus, consectetur non blandit in, vehicula 
+                sit amet sapien. Vestibulum dictum augue id commodo aliquam. 
+                Integer aliquet iaculis dapibus. Pellentesque est sapien, 
+                imperdiet non mi ut, rutrum commodo nisi. Mauris gravida 
+                convallis mauris, vitae posuere mauris rhoncus vitae. </p>
+        </div>
+        <div class="tredjedel tredjedel-siste kollaps">
+            <img src="Bilder/IS09AR7JD_Comp.jpg" alt="Eksempelbilde"/>
+            <h2>Trenger nytt hjem</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                dictum sodales tempus. Pellentesque fringilla ex a malesuada 
+                dictum. Proin enim purus, consectetur non blandit in, vehicula 
+                sit amet sapien. Vestibulum dictum augue id commodo aliquam. 
+                Integer aliquet iaculis dapibus. Pellentesque est sapien, 
+                imperdiet non mi ut, rutrum commodo nisi. Mauris gravida 
+                convallis mauris, vitae posuere mauris rhoncus vitae. </p>
+        </div>        
+                </div>
+        </div>
+    </body>
+</html>
